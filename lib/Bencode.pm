c6d4c14b0cdda0ae2448bd2127886e29f2d15ceb
Aristotle Pagaltzis <pagaltzis@gmx.de>
2015-01-04 06:34:06 +0100

    specify minimum Perl explicitly

--- lib/Bencode.pm
+++ lib/Bencode.pm
@@ -1,14 +1,17 @@
-package Bencode;
+use 5.006;
 use strict;
+use warnings;
+
+package Bencode;
 use Carp;
 use Exporter;
 
 # ABSTRACT: BitTorrent serialisation format
 
-use vars qw( $VERSION @ISA @EXPORT_OK $DEBUG $do_lenient_decode $max_depth );
+our ( $DEBUG, $do_lenient_decode, $max_depth );
 
-@ISA = qw( Exporter );
-@EXPORT_OK = qw( bencode bdecode );
+our @ISA = qw( Exporter );
+our @EXPORT_OK = qw( bencode bdecode );
 
 sub _msg { sprintf "@_", pos() || 0 }
 
